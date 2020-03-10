5c2ac2b35631defe4ee4a5807249fd1af833f8a4
Keith Woods <devshop7@gmail.com>
2020-03-10 10:14:28 +0800

    adding a watch build

--- index.ts
+++ index.ts
@@ -1,15 +1,11 @@
 import {pipe, Observable, of} from 'rxjs';
 
-// pipe(
-//     of(1, 2, 3),
-// )
-
 export function doOnSubscribe<T>(action: () => void) : (source: Observable<T>) => Observable<T> {
     return (source: Observable<T>) => new Observable<T>((subscriber) => {
         action();
         let subscription = source.subscribe(subscriber);
         return () => {
-            console.log(`Unsubscribing`);
+            console.log(`Unsubscribed`);
             subscription.unsubscribe();
         };
     });
